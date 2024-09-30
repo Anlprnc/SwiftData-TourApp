29e0909278cdabf2015b708a7f3fbc4585f6fedf
Anlprnc <anilpirincciii@gmail.com>
2024-09-30 22:14:56 +0300

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Anıl on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
